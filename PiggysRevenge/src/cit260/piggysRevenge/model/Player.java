0b6bd18db885d74a451f8c6e8d2ee1a30e781e9e
natebolton <nate@natebolton.com>
2017-01-25 18:35:44 -0800

    Added my name to the author section

--- PiggysRevenge/src/cit260/piggysRevenge/model/Player.java
+++ PiggysRevenge/src/cit260/piggysRevenge/model/Player.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 import java.util.Objects;
 /**
  *
- * @author hales
+ * @author hales AND bolton
  */
 public class Player implements Serializable{
     private String name;
