bf01bf024157c24ce889d2d56f0967be5e7fd663
natebolton <nate@natebolton.com>
2017-02-09 08:27:53 -0800

    Updated GameControlTest and GameControl

--- PiggysRevenge/src/cit260/piggysRevenge/control/GameControl.java
+++ PiggysRevenge/src/cit260/piggysRevenge/control/GameControl.java
@@ -10,25 +10,35 @@ package cit260.piggysRevenge.control;
  * @author natebolton
  */
 public class GameControl {
-    
-    public static int calcScore(int numberOfBricks, int numberOfTurns, boolean hasEaten, boolean wolfKilled) {
-    if (numberOfTurns <= 0){
-        return -1;}
-    if (numberOfBricks < 0){
-        return -2;}
-    
-    int currentScore;
-    
-    currentScore = (numberOfBricks * 10);
-    if  (hasEaten == true) {
-        currentScore = currentScore + 1000;
- 	if (wolfKilled == true){
-	currentScore = currentScore + 2000;}
-}
-    if (currentScore == 0){
-        return 0;}
-    else{
-	return currentScore - (numberOfTurns * 10);}
-}
-}
 
+    public static int calcScore(int numberOfBricks, int numberOfTurns, boolean hasEaten, boolean wolfKilled) {
+        //bricks can not be less than 0
+        if (numberOfBricks < 0) {
+            return -1;
+        }        
+        //turns can not be less than 1
+        if (numberOfTurns < 1) {
+            return -2;
+        }
+        //wolf can't be killed if player has not eaten.
+        if (hasEaten == false && wolfKilled == true) {
+            return -3;
+        }
+
+        int currentScore;
+
+        //all score weights subject to change
+        currentScore = (numberOfBricks * 10);
+        if (hasEaten == true) {
+            currentScore = currentScore + 1000;
+            if (wolfKilled == true) {
+                currentScore = currentScore + 2000;
+            }
+        }
+        if (currentScore == 0) {
+            return 0;
+        } else {
+            return currentScore - (numberOfTurns * 10);
+        }
+    }
+}
--- PiggysRevenge/test/cit260/piggysRevenge/control/GameControlTest.java
+++ PiggysRevenge/test/cit260/piggysRevenge/control/GameControlTest.java
@@ -22,36 +22,62 @@ public class GameControlTest {
      */
     @Test
     public void testCalcScore() {
-        System.out.println("Test1 - calcScore");
+        System.out.println("Test1 - Valid1");
         int numberOfBricks = 250;
         int numberOfTurns = 35;
-        boolean hasEaten = true;
+        boolean hasEaten = false;
         boolean wolfKilled = false;
-        int expResult = 3150;
+        int expResult = 2150;
         int result = GameControl.calcScore(numberOfBricks, numberOfTurns, hasEaten, wolfKilled);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         // fail("The test case is a prototype.");
-        
-        System.out.println("Test2 - numberOfBricks");
-        numberOfBricks = -1;
+        System.out.println("Test2 - Valid2");
+        numberOfBricks = 250;
         numberOfTurns = 35;
         hasEaten = true;
         wolfKilled = false;
-        expResult = -2;
+        expResult = 3150;
+        result = GameControl.calcScore(numberOfBricks, numberOfTurns, hasEaten, wolfKilled);
+        assertEquals(expResult, result);
+
+        System.out.println("Test3 - Valid3");
+        numberOfBricks = 250;
+        numberOfTurns = 35;
+        hasEaten = true;
+        wolfKilled = true;
+        expResult = 5150;
         result = GameControl.calcScore(numberOfBricks, numberOfTurns, hasEaten, wolfKilled);
         assertEquals(expResult, result);
         
-        System.out.println("Test3 - numberOfTurns");
-        numberOfBricks = 300;
-        numberOfTurns = -5;
-        hasEaten = false;
+        System.out.println("Test4 - invalid1 - numberOfBricks");
+        numberOfBricks = -1;
+        numberOfTurns = 35;
+        hasEaten = true;
         wolfKilled = false;
         expResult = -1;
         result = GameControl.calcScore(numberOfBricks, numberOfTurns, hasEaten, wolfKilled);
         assertEquals(expResult, result);
         
-        System.out.println("Test4 - 0 Score");
+        System.out.println("Test5 - invalid2 - numberOfTurns");
+        numberOfBricks = 300;
+        numberOfTurns = -5;
+        hasEaten = false;
+        wolfKilled = false;
+        expResult = -2;
+        result = GameControl.calcScore(numberOfBricks, numberOfTurns, hasEaten, wolfKilled);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test6 - invalid3 - wolfkilled but not eaten");
+        numberOfBricks = 250;
+        numberOfTurns = 35;
+        hasEaten = false;
+        wolfKilled = true;
+        expResult = -3;
+        result = GameControl.calcScore(numberOfBricks, numberOfTurns, hasEaten, wolfKilled);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test4 - lower boundary - 0 Score");
         numberOfBricks = 0;
         numberOfTurns = 1;
         hasEaten = false;
