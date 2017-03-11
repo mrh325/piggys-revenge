30e07b8ecd6fed9bcf51984a86e85eb3ef5721e7
mrh325 <mrh325@gmail.com>
2017-03-10 17:03:32 -0800

    Added Actor class

--- PiggysRevenge/src/cit260/piggysRevenge/model/Actor.java
+++ PiggysRevenge/src/cit260/piggysRevenge/model/Actor.java
@@ -5,12 +5,34 @@
  */
 package cit260.piggysRevenge.model;
 
+import java.awt.Point;
 import java.io.Serializable;
 
 /**
  *
  * @author hales
  */
+
 public enum Actor implements Serializable {
     
+    Piggy1("This little piggy went to market."),
+    Piggy2("This little piggy stayed home."),
+    Piggy3("This little piggy had roast beef."),
+    Piggy4("This little piggy had none.");
+    
+    private final String description;
+    private final Point coordinates;
+
+    Actor(String description) {
+        this.description = description;
+        coordinates = new Point (1,1);
+}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Point getCoordinates() {
+        return coordinates;
+    }
 }
